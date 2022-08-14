895234de5299f2c1be96bb8b485fe351beed4cc3
vladislav.borejsho <bjiadisjiav@gmail.com>
2022-08-14 23:34:35 +0300

    feat: added weather, slider, quotes.json

--- script.js
+++ script.js
@@ -24,43 +24,137 @@ function showDate()  {
 
 function showGreeting () {
     const greeting = document.querySelector('.greeting')
-    const timeOfDay = getTimeofDay();
+    let timeOfDay = getTimeofDay();
     const greetingText = `Good ${timeOfDay}`;
-    greeting.textContent = greetingText;
-       function getTimeofDay() {
-       const date = new Date();
-       const hours = date.getHours();
-       const min = date.getMinutes();
-       let greeting = '';
-       if (hours > -1 && hours < 6) {
-           greeting += 'night';
-       } 
-       else if (hours > 5 && hours < 12) {
-           greeting += 'morning';
-       } 
-       else if (hours > 11 && hours < 18) {
-           greeting += 'afternoon';
-       }
-       else if (hours > 17 && hours < 24) {
-           greeting += 'evening';
-       }
-       return greeting;
-   }
+    greeting.textContent = greetingText;     
+    return timeOfDay;
 }
+function getTimeofDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    const min = date.getMinutes();
+    let greeting = '';
+    if (hours > -1 && hours < 6) {
+        greeting += 'night';
+    } 
+    else if (hours > 5 && hours < 12) {
+        greeting += 'morning';
+    } 
+    else if (hours > 11 && hours < 18) {
+        greeting += 'afternoon';
+    }
+    else if (hours > 17 && hours < 24) {
+        greeting += 'evening';
+    }
+    return greeting;
+    } 
 
-function setLocalStorage () {
+function setLocalStorageName () {
     const inputName = document.querySelector('.name')
     localStorage.setItem('name', inputName.value)
 }
-window.addEventListener('beforeunload', setLocalStorage)
+window.addEventListener('beforeunload', setLocalStorageName)
 
-function getLocalStorage() {
+function getLocalStorageName() {
     if (localStorage.getItem('name')) {
         inputName.value = localStorage.getItem('name');
     }
 }
-window.addEventListener('load', getLocalStorage)
+window.addEventListener('load', getLocalStorageName)
 
                     // 3. Слайдер изображений //
+const body = document.querySelector('body')
+const slideNext = document.querySelector('.slide-next')
+const slidePrev = document.querySelector('.slide-prev')
+    let timeOfDay = getTimeofDay();
+    let randomNum = getRandomNum(1,20);
 
-body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/01.jpg')";
+function getRandomNum(min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min
+}
+
+function setBg() {
+    const img = new Image();
+    let randNum = randomNum.toString().padStart(2,'0');
+
+    img.src = `https://raw.githubusercontent.com/vladislav-borejsho/Momentum/main/assets/img/${timeOfDay}/${randNum}.jpg`;
+    img.onload = () => {
+        body.style.backgroundImage = `url(${img.src})`;
+    }
+}
+setBg()
+
+function getSlideNext () {
+    if (randomNum < 20) {
+        randomNum += 1;
+    } else {
+        randomNum = 1;
+    }
+    setBg()
+}
+function getSlidePrev () {
+    if (randomNum > 2) {
+        randomNum -= 1;
+    } else {
+        randomNum = 20;
+    }
+    setBg()
+}
+slideNext.addEventListener('click', getSlideNext)
+slidePrev.addEventListener('click', getSlidePrev)
+
+                        // 4. Виджет погоды //
+
+const weatherIcon = document.querySelector('.weather-icon')
+const temperature = document.querySelector('.temperature')
+const weatherDescription = document.querySelector('.weather-description')
+const weatherError = document.querySelector('.weather-error')
+const wind = document.querySelector('.wind')
+const humidity = document.querySelector('.humidity')
+const city = document.querySelector('.city')
+city.value = getLocalStorageCity()
+
+city.addEventListener('change', getWeather)
+
+async function getWeather() {        
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=d5e6290f7b9ec107a419737efbb3ff86&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    try {
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${Math.ceil(data.main.temp)}°C`;
+        weatherDescription.textContent = data.weather[0].description; 
+        humidity.textContent = `Humidity: ${data.main.humidity}%`;
+        wind.textContent = `Wind speed: ${Math.round(data.wind.speed)}m/s`;
+        weatherError.textContent = '';
+    }
+    catch (er) {
+        weatherIcon.className = '';
+        temperature.textContent = '';
+        humidity.textContent = '';
+        wind.textContent = '';
+        weatherDescription.textContent = '';
+        weatherError.textContent = data.message;
+    }
+}
+getWeather()
+
+function setLocalStorageCity () {
+    const city = document.querySelector('.city')
+    localStorage.setItem('city', city.value)
+}
+window.addEventListener('beforeunload', setLocalStorageCity)
+
+function getLocalStorageCity() {
+    if (localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city');
+    } else { 
+        city.value = 'Minsk';
+    }
+    return city.value;
+}
+window.addEventListener('load', getLocalStorageCity)
